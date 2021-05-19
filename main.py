a47c06f438564e9f3c06007f9a1aa3dab8c47e02
Sho Yano <takamatsu.shyo@gmail.com>
2021-05-19 19:52:45 +0300

    record 3d image flow

--- main.py
+++ main.py
@@ -7,22 +7,35 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     logger = logging.getLogger('main')
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     logger.info("info")
 
     vid = cv2.VideoCapture(0)
     ret, frame = vid.read()
- 
+
+    stacked_frame = np.zeros((300,400),)
+    logger.debug(f"0 {stacked_frame.shape}")
+
+
     while(ret):
         ret, frame = vid.read()
         frame = cv2.resize(frame, (400,300))
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret,frame = cv2.threshold(frame,128,255,cv2.THRESH_BINARY)
+        #ret,frame = cv2.threshold(frame,128,255,cv2.THRESH_BINARY)
 
-        plt.imshow(frame)
-        plt.pause(0.01)
+        stacked_frame = np.dstack((stacked_frame, frame))
+        logger.debug(f"loop {stacked_frame.shape}")
+        last_frame_number = stacked_frame.shape[2]
+        cv2.imshow("frame", frame)
 
-        if cv2.waitKey(1)  & 0xFF == ord('q'):
+        if last_frame_number > 100:
+            import pickle 
+            filehandler = open('3d-flow.pkl', 'w') 
+            pickle.dump(stacked_frame, filehandler)
+            break
+
+
+        if cv2.waitKey(100)  & 0xFF == ord('q'):
             vid.release()
             ret = False
             break
